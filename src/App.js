21911e2e13890a768e54e2293def795202deb2e5
JatinBhargava <63741896+JatinBhargava@users.noreply.github.com>
2021-11-11 17:21:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import data from "./Data";
+import List from "./List";
+import { useState } from "react";
+
+export default function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <div className="main">
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </div>
+  );
+}
